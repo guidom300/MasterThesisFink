30689094e61285995d0d187e452182937694cd00
phil <philjjoon@gmail.com>
2015-11-03 11:23:48 +0100

    query 12 update

--- query-scala/src/main/scala/de/tub/cs/bigbench/Query12.scala
+++ query-scala/src/main/scala/de/tub/cs/bigbench/Query12.scala
@@ -2,81 +2,62 @@ package de.tub.cs.bigbench
 
 import java.text.SimpleDateFormat
 
+import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
 import org.apache.flink.api.table.expressions.Avg
 
 /*
  * TODO
  * Analyze each required column based on 4 tables
- * set q12_startDate=2001-09-02;
+set q12_startDate=2001-09-02;
 set q12_endDate1=2001-10-02;
 set q12_endDate2=2001-12-02;
 set q12_i_category_IN='Books', 'Electronics';
- *
+
+val dateFormat = new SimpleDateFormat("yyy-MM-dd")
+val startDate = dateFormat.parse(startDate)
+val endDate1 = dateFormat.parse(endData1)
+val endDate2 = dateFormat.parse(endData2 )
+
+USAGE
+.filter( l => dateFormat.prase(l.shipDate).after(startDate)
+.filter( l => dateFormat.prase(l.shipDate).before(startDate)
+
  */
 
 object Query12{
+
+  // arg_configuration
+  val startDate = "2001-09-02"
+  val endData1 = "2001-10-02"
+  val endData2 = "2001-12-02"
+  val i_category_IN1 = "Books"
+  val i_category_IN2 = "Electronics"
 
   def main(args: Array[String]) {
     if (!parseParameters(args)) {
       return
     }
 
-    // set filter date
-    val dateFormat = new SimpleDateFormat("yyy-MM-dd")
-    val startDate111 = dateFormat.parse("2001-09-02")
-    val endDate1 = dateFormat.parse("2001-10-02")
-    val endDate2 = dateFormat.parse("2001-12-02")
-
-    /*
-    USAGE
-    .filter( l => dateFormat.prase(l.shipDate).after(startDate)
-    .filter( l => dateFormat.prase(l.shipDate).before(startDate)
-
-     */
-
-
     // set up execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
 
-    val filteredItemTable = getItemDataSet(env)
-    /*
-    AND i_category IN (${hiveconf:q12_i_category_IN}) -- filter given category
-     */
+    val filteredItemTable = getItemDataSet(env).filter(items => items._category.equals(i_category_IN1) || items._category.equals(i_category_IN2))
+    val filteredWebClickTable = getWebClickDataSet(env).filter(items => items._click_date >= 37134 && items._click_date <= 37164 && items._user_sk != null && items._sales_sk == null)
+    val storeTable = getStoreSalesDataSet(env).filter(items => items._sold_date_sk >= 37134 && items._sold_date_sk <= 37224 && items._customer_sk != null)
 
-    val filteredwebClickTable = getWebClickDataSet(env)
-    /*
-    WHERE wcs_click_date_sk BETWEEN 37134 AND (37134 + 30) -- in a given month and year
+    val webInRange = filteredWebClickTable.join(filteredItemTable).where(_._item_sk).equalTo(_._item_sk).apply((wc,i) => (wc))
+    val storeInRange = storeTable.join(filteredItemTable).where(_._item_sk).equalTo(_._item_sk).apply((st,i) => (st))
 
-    AND wcs_user_sk IS NOT NULL
-    AND wcs_sales_sk IS NULL --only views, not purchases
-     */
-    val storeTable = getStoreSalesDataSet(env)
-    /*
-    WHERE ss_sold_date_sk BETWEEN 37134 AND (37134 + 90) -- in the three consecutive months.
+    val realQuery = webInRange.join(storeInRange).where(_._user_sk).equalTo(_._customer_sk).apply((wc,st) => (wc._user_sk,wc._click_date,st._sold_date_sk))
+      .filter(items => items._2 < items._3)
+      .sortPartition(0,Order.ASCENDING)         // OREDER BY _user_sk
+      .setParallelism(1)
+      .distinct(0)                              // SELECT DISTINCT _user_sk
 
-    AND ss_customer_sk IS NOT NULL
-    */
+    realQuery.print()
 
-    val webInRange =
-    /*
-    **AND wcs_item_sk = i_item_sk
-    Join webClick and Item where item == item
-     */
-
-    val storeInRange =
-    /*
-    **AND ss_item_sk = i_item_sk
-    Join Sales and Item where item == item
-     */
-
-    val realQuery =
-    /*
-    Join webInRange and storeInRange where user == customer && click_date < ss_sold_date
-    .sortPartition("user").setParallism(1)
-    .flatMap(_.distinct)
-
-     */
+    env.execute("Scala Query 14 Example")
 
   }
 
@@ -92,11 +73,11 @@ object Query12{
   // _item_sk(0), _current_price(5), _category(12)
   case class Item(_item_sk: Long, _current_price: Double, _category: String)
 
-  // _click_date_sk(0), _user_sk(5), _item_sk(3), _sales_sk(2)
-  case class WebClick(click_date: Long, click_time: Long, user: Int, item: Int)
+  // _click_date_sk(0), _sales_sk(2), _item_sk(3), _user_sk(5),
+  case class WebClick(_click_date: Long, _sales_sk:Long, _item_sk: Int, _user_sk: Int)
 
   //_customer_sk(3), _item_sk(2),_sold_date_sk(0)
-  case class Store_sales(_sold_date_sk: Long, _item_sk: Long, _customer_sk: Long)
+  case class StoreSales(_sold_date_sk: Long, _item_sk: Long, _customer_sk: Long)
 
 
   // *************************************************************************
@@ -141,8 +122,8 @@ object Query12{
   }
 
 
-  private def getStoreSalesDataSet(env: ExecutionEnvironment): DataSet[Store_sales] = {
-    env.readCsvFile[Store_sales](
+  private def getStoreSalesDataSet(env: ExecutionEnvironment): DataSet[StoreSales] = {
+    env.readCsvFile[StoreSales](
       storeSalesPath,
       fieldDelimiter = "|",
       includedFields = Array(0, 2, 3),
@@ -155,3 +136,15 @@ object Query12{
 class Query12 {
 
 }
+
+/*
+1)
+AND i_category IN (${hiveconf:q12_i_category_IN}) -- filter given category
+2)
+WHERE wcs_click_date_sk BETWEEN 37134 AND (37134 + 30) -- in a given month and year
+AND wcs_user_sk IS NOT NULL
+3)
+AND wcs_sales_sk IS NULL --only views, not purchases
+WHERE ss_sold_date_sk BETWEEN 37134 AND (37134 + 90) -- in the three consecutive months.
+AND ss_customer_sk IS NOT NULL
+*/
