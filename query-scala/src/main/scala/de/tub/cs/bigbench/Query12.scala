d8540ad873fb3883c2ad8ea133e7cc955215e11f
phil <philjjoon@gmail.com>
2015-11-06 09:18:57 +0100

    handling null value query 12 with the column of web_sales

--- query-scala/src/main/scala/de/tub/cs/bigbench/Query12.scala
+++ query-scala/src/main/scala/de/tub/cs/bigbench/Query12.scala
@@ -1,29 +1,16 @@
 package de.tub.cs.bigbench
 
-import java.text.SimpleDateFormat
-
 import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
 import org.apache.flink.api.scala._
-import org.apache.flink.api.table.expressions.Avg
+
 
 /*
+ * Edit Configuration
+ * "/home/jjoon/bigBench/data-generator/output/item.dat" "/home/jjoon/bigBench/data-generator/output/web_clickstreams.dat" "/home/jjoon/bigBench/data-generator/output/store_sales.dat" "/home/jjoon/bigBench/"
  * TODO
  * Analyze each required column based on 4 tables
-set q12_startDate=2001-09-02;
-set q12_endDate1=2001-10-02;
-set q12_endDate2=2001-12-02;
-set q12_i_category_IN='Books', 'Electronics';
-
-val dateFormat = new SimpleDateFormat("yyy-MM-dd")
-val startDate = dateFormat.parse(startDate)
-val endDate1 = dateFormat.parse(endData1)
-val endDate2 = dateFormat.parse(endData2 )
-
-USAGE
-.filter( l => dateFormat.prase(l.shipDate).after(startDate)
-.filter( l => dateFormat.prase(l.shipDate).before(startDate)
-
+ * Handling null-value with the column of web_sales
  */
 
 object Query12{
@@ -40,11 +27,10 @@ object Query12{
       return
     }
 
-    // set up execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
 
     val filteredItemTable = getItemDataSet(env).filter(items => items._category.equals(i_category_IN1) || items._category.equals(i_category_IN2))
-    val filteredWebClickTable = getWebClickDataSet(env).filter(items => items._click_date >= 37134 && items._click_date <= 37164 && items._user_sk != null && items._sales_sk == null)
+    val filteredWebClickTable = getWebClickDataSet(env).filter(items => items._click_date >= 37134 && items._click_date <= 37164)// && items._user_sk != null && items._sales_sk == null)
     val storeTable = getStoreSalesDataSet(env).filter(items => items._sold_date_sk >= 37134 && items._sold_date_sk <= 37224 && items._customer_sk != null)
 
     val webInRange = filteredWebClickTable.join(filteredItemTable).where(_._item_sk).equalTo(_._item_sk).apply((wc,i) => (wc))
@@ -52,14 +38,16 @@ object Query12{
 
     val realQuery = webInRange.join(storeInRange).where(_._user_sk).equalTo(_._customer_sk).apply((wc,st) => (wc._user_sk,wc._click_date,st._sold_date_sk))
       .filter(items => items._2 < items._3)
+      .map(items => Tuple1(items._1))
       .sortPartition(0,Order.ASCENDING)         // OREDER BY _user_sk
       .setParallelism(1)
       .distinct(0)                              // SELECT DISTINCT _user_sk
 
-    realQuery.print()
+    .count()            // must be 403
+    println(realQuery)
+    //realQuery.print()
 
-    env.execute("Scala Query 14 Example")
-
+    //env.execute("Scala Query 14 Example")
   }
 
 
@@ -75,7 +63,7 @@ object Query12{
   case class Item(_item_sk: Long, _current_price: Double, _category: String)
 
   // _click_date_sk(0), _sales_sk(2), _item_sk(3), _user_sk(5),
-  case class WebClick(_click_date: Long, _sales_sk:Long, _item_sk: Int, _user_sk: Int)
+  case class WebClick(_click_date: Long, _sales_sk:Long, _item_sk: Long, _user_sk: Long)
 
   //_customer_sk(3), _item_sk(2),_sold_date_sk(0)
   case class StoreSales(_sold_date_sk: Long, _item_sk: Long, _customer_sk: Long)
@@ -148,4 +136,19 @@ AND wcs_user_sk IS NOT NULL
 AND wcs_sales_sk IS NULL --only views, not purchases
 WHERE ss_sold_date_sk BETWEEN 37134 AND (37134 + 90) -- in the three consecutive months.
 AND ss_customer_sk IS NOT NULL
+
+set q12_startDate=2001-09-02;
+set q12_endDate1=2001-10-02;
+set q12_endDate2=2001-12-02;
+set q12_i_category_IN='Books', 'Electronics';
+
+val dateFormat = new SimpleDateFormat("yyy-MM-dd")
+val startDate = dateFormat.parse(startDate)
+val endDate1 = dateFormat.parse(endData1)
+val endDate2 = dateFormat.parse(endData2 )
+
+USAGE
+.filter( l => dateFormat.prase(l.shipDate).after(startDate)
+.filter( l => dateFormat.prase(l.shipDate).before(startDate)
+
 */
